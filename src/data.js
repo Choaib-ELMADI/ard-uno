1ccb86e809b65ef0b4130865b2c91e2185d01531
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-03 21:40:16 +0100

    Create some data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+import { images } from "./constants/index";
+
+export const sponsors = [
+    {
+        name: 'Arduino',
+        link: 'https://www.arduino.cc/',
+        img : images.arduino_logo,
+    },
+    {
+        name: 'ArduinoPlanet',
+        link: 'https://www.arduiplanet.ma/',
+        img : images.arduinoPlanet_logo,
+    },
+    {
+        name: 'EasyEDA',
+        link: 'https://easyeda.com/fr',
+        img : images.easyEDA_logo,
+    },
+];
