52c480ff370acc82be2265053089e1ab01f1ccde
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-03 15:11:19 +0100

    Navbar ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { MdOutlineMenuOpen } from 'react-icons/md';
-import { FaWindowClose } from 'react-icons/fa';
+import { IoClose } from 'react-icons/io5';
 
 import './Navbar.css';
-const main = ['Services', 'Work', 'About', 'Pricing', 'Pages', 'Cart'];
+const main = ['Services', 'Work', 'About', 'Pricing', 'Pages'];
 
 
 
@@ -16,28 +16,32 @@ const Navbar = () => {
                 <a 
                     href='/' 
                     className='app__logo'
-                >leevi</a>
-                <ul className={ open ? 'app__navbar-main active' : 'app__navbar-main' }>
-                    {
-                        main.map(item => (
-                            <li 
-                                key={ item }
-                                onClick={ () => setOpen(false) }
-                            >
-                                <a href={ `#${ item }` }>{ item }</a>
-                            </li>
-                        ))
-                    }
-                </ul>
+                >
+                    <h1>Arduno</h1>
+                </a>
+                <div className={ open ? 'app__navbar-container active' : 'app__navbar-container' }>
+                    <ul className='app__navbar-main active'>
+                        {
+                            main.map(item => (
+                                <li 
+                                    key={ item }
+                                    onClick={ () => setOpen(false) }
+                                >
+                                    <a href={ `#${ item }` }><h4>{ item }</h4></a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <button className='app__navbar-btn'>Let's Talk</button>
                 <button 
                     onClick={ () => setOpen(!open) }
                     className='toggle-navbar'
                 >
                     {
-                        !open ? <MdOutlineMenuOpen fontSize={ 30 } /> : <FaWindowClose fontSize={ 30 } />
+                        !open ? <MdOutlineMenuOpen fontSize={ 32 } /> : <IoClose fontSize={ 32 } />
                     }
                 </button>
-                <button className='app__navbar-btn'>Let's Talk</button>
             </ul>
         </nav>
     );
