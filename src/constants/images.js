86637e6b67ceef84933e931b517fb6ee0614715e
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-03 21:39:12 +0100

    Add some assets

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+import banner             from '../assets/banner.webp';
+
+import arduino_logo       from '../assets/arduino-logo.png';
+import easyEDA_logo       from '../assets/EasyEDA.png';
+import arduinoPlanet_logo from '../assets/arduinoPlanet.png.webp';
+
+export default {
+    banner,
+
+    arduino_logo,
+    easyEDA_logo,
+    arduinoPlanet_logo,
+}
