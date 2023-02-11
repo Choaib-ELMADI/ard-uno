2d0a8172575b0d6e8222056c3eea349cc65d3a1b
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-11 14:11:02 +0100

    Setting up firebase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+
 import './index.css';
 import App from './App';
+
+
+
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyCRlh5P8-fcjP7H2yg2lz2iQLA-FBnp0do",
+  authDomain: "ard-uno.firebaseapp.com",
+  projectId: "ard-uno",
+  storageBucket: "ard-uno.appspot.com",
+  messagingSenderId: "324478082220",
+  appId: "1:324478082220:web:58af7b6412d3c2427a1309"
+};
+initializeApp(firebaseConfig);
 
 
 
